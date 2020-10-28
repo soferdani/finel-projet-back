d3c94358bde03dd924278352a56206a827cd3d63
yuvalotem <yuvalotem@gmail.com>
2020-10-28 16:49:04 +0200

    todo and fixig routes

--- server/db/DataBaseServices.js
+++ server/db/DataBaseServices.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Sequelize = require('sequelize')
 const moment = require('moment')
 
-const sequelize = new Sequelize(`mysql://admin:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/final_project`)
+const sequelize = new Sequelize(`mysql://admin:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/manageme`)
 
 sequelize
     .authenticate()
@@ -16,16 +16,16 @@ sequelize
 const DBServices = function () {
 
     //user
-    const getUser = async (email, password) => {
-        const query = `SELECT * FROM USER
-        WHERE email = '${email}'
-        AND user_pass = '${password}';`
+    const getUser = async (email) => {
+        const query = `SELECT user_id, first_name, last_name, email, phone, datejoin, user_type, token, img
+        FROM user
+        WHERE email = '${email}';`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const saveUser = async (user) => {
-        const query = `INSERT INTO USER VALUES(
+        const query = `INSERT INTO user VALUES(
             null,
             '${user.firstName}',
             '${user.lastName}',
@@ -35,42 +35,45 @@ const DBServices = function () {
             '${user.password}',
             '${user.type}',
             '${user.token}',
-            '${user.avatar}');`
+            '${user.img}');`
         const id = await sequelize.query(query)
         return id
     }
 
     const updateUser = async (id, userData) => {
-        const query = `UPDATE USER
-        SET email = '${userData.email}',
-        first_name = '${userData.firstName}',
-        last_name = '${userData.lastName}',
-        phone = '${userData.phone}',
-        user_pass = '${userData.password}',
-        user_type = '${userData.type}'
-        WHERE user_id = ${id}`
-        const responseFromDB = await sequelize.query(query)
+        let query = `UPDATE user SET `
+        for (let i in Object.keys(userData)) {
+            const key = Object.keys(userData)[i]
+            const endChar = i == Object.keys(userData).length - 1 ? '' : ','
+            if (typeof userData[key] === "string") {
+                query += `${key} = '${userData[key]}'${endChar} `
+            } else {
+                query += `${key} = ${userData[key]}${endChar} `
+            }
+        }
+        query += `WHERE user_id = ${id}`
+        const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const deleteUser = async (id) => {
-        const query = `DELETE FROM USER WHERE user_id = ${id}`
+        const query = `DELETE FROM user WHERE user_id = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     //properties
     const getProperties = async (id) => {
-        const query = `SELECT *
-        FROM PROPERTY, OWNERS
-       WHERE PROPERTY.ONWER = OWNERS.id
-       AND PROPERTY.maneger = ${id};`
+        const query = `SELECT id, img, adress, num_roms, bathroms, max_gusts, pool, ac, wifi,kitchen, owner, name, phone, country, email
+        FROM property AS p JOIN property_user AS po ON p.id = po.property
+        JOIN owner AS o ON o.o_id = p.owner
+        WHERE po.user = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const saveProperty = async (propertie) => {
-        const query = `INSERT INTO PROPERTY VALUES(
+        const query = `INSERT INTO property VALUES(
         null,
         '${propertie.img}',
         '${propertie.adress}',
@@ -81,68 +84,88 @@ const DBServices = function () {
         ${propertie.ac},
         ${propertie.wifi},
         ${propertie.kitchen},
-        ${propertie.owner},
-        ${propertie.manager})`
+        ${propertie.owner})`
         const id = await sequelize.query(query)
+        const joinQuery = `INSERT INTO property_user VALUES(
+            ${propertie.manager},
+            ${id[0]})`
+        await sequelize.query(joinQuery)
         return id
     }
 
     const updateProperty = async (propertyData, id) => {
-        const query = `UPDATE PROPERTY
-        SET ac = ${propertyData.ac},
-        adress = '${propertyData.adress}',
-        bathroms = ${propertyData.bathrooms},
-        img = '${propertyData.img}',
-        kitchen = ${propertyData.kitchen},
-        maneger = ${propertyData.manager},
-        max_gusts = ${propertyData.maxGusts},
-        num_roms = ${propertyData.numRooms},
-        onwer = ${propertyData.owner},
-        pool = ${propertyData.pool},
-        wifi = ${propertyData.wifi}
-        WHERE id = ${id}`
+        let query = `UPDATE property SET `
+        for (let i in Object.keys(propertyData)) {
+            const key = Object.keys(propertyData)[i]
+            const endChar = i == Object.keys(propertyData).length - 1 ? '' : ','
+            if (typeof propertyData[key] === "string") {
+                query += `${key} = '${propertyData[key]}'${endChar} `
+            } else {
+                query += `${key} = ${propertyData[key]}${endChar} `
+            }
+        }
+        query += `WHERE id = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const deleteProperty = async (id) => {
-        const query = `DELETE FROM PROPERTY WHERE id = ${id}`
+        await sequelize.query(`DELETE FROM todo WHERE property = ${id}`)
+        await sequelize.query(`DELETE FROM property_user WHERE property = ${id}`)
+        const query = `DELETE FROM property WHERE id = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     //todos
     const getTodos = async (id) => {
-        const query = `SELECT *
-        FROM TODO, SERVICE_TYPE
-        WHERE TODO.service_provider_type = SERVICE_TYPE.id
-        AND TODO.property = ${id};`
+        const query = `SELECT t.t_id, task, is_complete, create_date, type_name, img
+        FROM todo AS t JOIN user_type AS ut
+        on t.service_type = ut.type_id
+        WHERE t.property = ${id};`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const saveTodo = async (todo) => {
-        const query = `INSERT INTO TODO VALUES(
+        const query = `INSERT INTO todo VALUES(
         null,
         '${todo.task}',
         ${todo.property},
         ${todo.type},
-        false);`
-        //'${moment().format('YYYY-MM-DD')}',
+        '${moment().format('YYYY-MM-DD')}',
+        false,
+        '${todo.img}');`
         const id = await sequelize.query(query)
         return id
     }
 
-    const updateTodo = async (id) => {
-        const query = `UPDATE TODO
-        SET  is_complite = 1
-        WHERE id = ${id}`
+    const checkTodo = async (id) => {
+        const query = `UPDATE todo
+        SET  is_complete = 1
+        WHERE t_id = ${id}`
+        const [responseFromDB] = await sequelize.query(query)
+        return responseFromDB
+    }
+
+    const updateTodo = async (id, todo) => {
+        let query = `UPDATE todo SET `
+        for (let i in Object.keys(todo)) {
+            const key = Object.keys(todo)[i]
+            const endChar = i == Object.keys(todo).length - 1 ? '' : ','
+            if (typeof todo[key] === "string") {
+                query += `${key} = '${todo[key]}'${endChar} `
+            } else {
+                query += `${key} = ${todo[key]}${endChar} `
+            }
+        }
+        query += `WHERE t_id = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
 
     const deleteTodo = async (id) => {
-        const query = `DELETE FROM todos WHERE id = ${id}`
+        const query = `DELETE FROM todo WHERE t_id = ${id}`
         const [responseFromDB] = await sequelize.query(query)
         return responseFromDB
     }
@@ -158,6 +181,7 @@ const DBServices = function () {
         deleteProperty,
         getTodos,
         saveTodo,
+        checkTodo,
         updateTodo,
         deleteTodo
     }
--- server/db/comends.sql
+++ server/db/comends.sql
@@ -1,10 +1,13 @@
-USE final_project;
+USE manageme;
 
 
--- SELECT * FROM USER_TYPE;
--- SELECT * FROM PROPERTY;
--- SELECT * FROM SERVICE_TYPE;
--- SELECT * FROM TODO;
+-- SELECT * FROM user;
+-- SELECT * FROM user_type;
+-- SELECT * FROM property;
+-- SELECT * FROM property_user;
+-- SELECT * FROM owner;
+-- SELECT * FROM booking;
+-- SELECT * FROM todo;
 -- DELETE FROM USER;
 
 -- INSERT INTO OWNERS VALUES(
@@ -19,6 +22,30 @@ USE final_project;
 -- WHERE PROPERTY.maneger = USER.user_id
 -- AND PROPERTY.maneger = 11;
 
--- INSERT INTO SERVICE_TYPE VALUES(
+-- INSERT INTO user_type VALUES(
 --         NULL,
---         'Plumbing');
+--         'plumbing');
+-- Electricity
+-- Plumbing
+-- SELECT p.id, img, adress, num_roms, bathroms, max_gusts, pool, ac, wifi,kitchen, onwer, maneger, name, phone, country, email
+--         FROM PROPERTY AS p JOIN OWNERS AS o
+--         ON p.ONWER = o.id
+--        WHERE p.maneger = 11;
+
+-- DELETE FROM USER_TYPE WHERE type_name = 'elctricety'
+
+-- ALTER TABLE TODO
+-- DROP service_provider_type;
+-- ALTER TABLE user COLUMN
+
+-- INSERT INTO owner VALUES(
+--         NULL,
+--         'Ido Zehavi',
+--         '0508991538',
+--         'Israel',
+--         'idozehavi123@gmail.com');
+
+-- delete from property
+
+-- delete from todo
+-- ALTER TABLE todo MODIFY t_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
