1bf5122cae2db7eefd9df8d413ec3c04739361d5
soferdani <soferdani@gmail.com>
2020-11-02 10:56:08 +0200

    split cron function

--- server/db/bookingServices.js
+++ server/db/bookingServices.js
@@ -1,9 +1,15 @@
 const sequelize = require('./sqlConnection')
-import axios from 'axios'
+const axios = require('axios')
 const cron = require('node-cron')
 const moment = require('moment')
 
 const bookingDBServices = function () {
+
+    const checkBooking = async (bookingId) => { //tested
+        bookingExist = await sequelize
+            .query(`SELECT * FROM booking WHERE id = '${bookingId}'`)
+        return bookingExist[0].length === 0
+    }
 
     const getAllBooking = async (propertyId) => { //tested
         let query = `select id,
@@ -19,22 +25,19 @@ const bookingDBServices = function () {
     }
 
     const saveBooking = async (booking) => { //tested
-        const query = `INSERT INTO booking VALUES(
-            null,
-            "${booking.startDate}",
-            "${booking.endDate}",
-            ${booking.propertyId},
-            ${booking.guests},
-            "${booking.channel}",
-            ${booking.nights},
-            "${booking.firstName}",
-            "${booking.lastName}",
-            "${booking.img}",
-            "${booking.exPropertyName}",
-            "${booking.phone}",
-            "${booking.email}");`
-        const responseFromDB = await sequelize.query(query)
-        return responseFromDB
+        let query = `insert into booking values (
+            "${booking.id}",
+            ${moment(booking.fromdate_c).format()}, 
+            ${moment(booking.todate_c).format()},
+            (select id from property where name = '${booking.CHINGEMEEE}'),
+            ${parseInt(booking.adults_c) + parseInt(booking.children_c)},
+            "${booking.lead_source}",
+            "${booking.phone_mobile}",
+            "${booking.email}",
+            "${booking.name}"                
+        );`
+        const newBooking = await sequelize.query(query)
+        return newBooking
     }
 
     const updateBooking = async (booking, id) => {
@@ -61,33 +64,9 @@ const bookingDBServices = function () {
         return responseFromDB
     }
 
-    cron.schedule('2 * * * *', async () => {
-        let newBookingFromAPI = axios.get('http://97.107.140.152/bookings_last_hour.php') //FIXME: TO MAKE SURE
-        let allBooking = []
-        for (let booking of newBookingFromAPI.data.data) {
-            let bookingExist = await sequelize
-                .query(`SELECT * FROM booking WHERE id = '${booking.id}'`) 
-            if (bookingExist[0].length === 0) {
-                let query = `insert into booking values (
-                    "${booking.id}",
-                    ${moment(booking.fromdate_c).format()}, 
-                    ${moment(booking.todate_c).format()},
-                    (select id from property where name = '${booking.CHINGEMEEE}'),
-                    ${parseInt(booking.adults_c) + parseInt(booking.children_c)},
-                    "${booking.lead_source}",
-                    "${booking.phone_mobile}",
-                    "${booking.email}",
-                    "${booking.name}"                
-                );`
-                const newBooking = await sequelize.query(query)
-                allBooking.push(newBooking)
-            }
-        }
-        return allBooking
-    })
-
 
     return {
+        checkBooking,
         saveBooking,
         updateBooking,
         getAllBooking,
