c5a2d689d0a0a3de26b5cd51c18d5463d48d2cbf
soferdani <soferdani@gmail.com>
2020-11-03 16:03:08 +0200

    add new servisers

--- server/db/comends.sql
+++ server/db/comends.sql
@@ -2,12 +2,14 @@ USE manageme;
 
 
 -- select * from property_user;
+-- select * from user;
 
-
+-- select * from property_user;
+-- select * from manger_employee;
 
 -- select * from user where user_type = 1
 
-ALTER TABLE user ADD api_getway text; 
+-- ALTER TABLE user ADD api_getway text; 
 -- select * from user;
 -- insert into property_user values (
 --     31,
